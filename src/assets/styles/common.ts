f4b661be7682b5771e57ed4b0693fa72fd3ac5e1
eridion semini <eridionsemini@gmail.com>
2024-04-17 20:51:55 +0200

    add comon styles

--- src/assets/styles/common.ts
+++ src/assets/styles/common.ts
@@ -1,27 +1,28 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import {colors} from 'cons/';
-import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+
+import { colors } from 'cons/';
 
 const commonStyles = StyleSheet.create({
-  input: {
-    height: 48,
-    borderColor: colors.GREEN_300,
-  },
-  container: {
-    flex: 1,
-    width: wp('100%') - 32,
-    marginHorizontal: 16,
-  },
-  flexRow: {
-    flexDirection: 'row',
-  },
-  itemsCenter: {
-    alignItems: 'center',
-  },
-  spaceBetween: {
-    justifyContent: 'space-between',
-  },
+    input: {
+        height: 48,
+        borderColor: colors.GREEN_300,
+    },
+    container: {
+        flex: 1,
+        width: wp('100%') - 32,
+        marginHorizontal: 16,
+    },
+    flexRow: {
+        flexDirection: 'row',
+    },
+    itemsCenter: {
+        alignItems: 'center',
+    },
+    spaceBetween: {
+        justifyContent: 'space-between',
+    },
 });
 
 export default commonStyles;

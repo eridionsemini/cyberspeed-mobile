5c02eb10bb9868a65f4ff7da7d4b6b2d29b375ca
eridion semini <eridionsemini@gmail.com>
2024-04-19 16:09:48 +0200

    add sdk package

--- App.tsx
+++ App.tsx
@@ -5,12 +5,16 @@ import {LogBox, Platform} from 'react-native';
 import NativeDevSettings from 'react-native/Libraries/NativeModules/specs/NativeDevSettings';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {MySDK} from 'movies-sdk';
 
 import RootNavi from 'navi/root';
 
 import {persistedStore, store} from './src/store';
 
 LogBox.ignoreAllLogs();
+
+const client = new MySDK();
+
 
 const connectToRemoteDebugger = () => {
   NativeDevSettings.setIsDebuggingRemotely(true);
